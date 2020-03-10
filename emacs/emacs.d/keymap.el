bfa0031771558b547a5ff0247008ca7717961929
Geoff Wilson <G.Wilson@F5.com>
2020-03-10 15:38:59 +0000

    Fix move-line-up/down keymaps

--- emacs/emacs.d/keymap.el
+++ emacs/emacs.d/keymap.el
@@ -1,8 +1,8 @@
 (global-set-key (kbd "M-Y") 'yank-pop-forwards)
 (global-set-key (kbd "C-l") 'duplicate-line)
 (global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "ESC <up>") 'move-line-up)
-(global-set-key (kbd "ESC <down>") 'move-line-down)
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
 (global-set-key (kbd "C-c s") 'ag)
 
 (global-set-key (kbd "C-c a") 'org-agenda)
