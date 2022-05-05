15db4e19ffb3f6fc46e5cc92d48af389d19808e1
Geoff Wilson <G.Wilson@F5.com>
2022-05-05 15:09:23 -0400

    Add up/down window scroll

--- emacs/emacs.d/ivy.el
+++ emacs/emacs.d/ivy.el
@@ -13,7 +13,6 @@
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
 (global-set-key (kbd "<f6>") 'ivy-resume)
 (global-set-key (kbd "M-x") 'counsel-M-x)
-(global-set-key (kbd "M-p") 'counsel-git)
 (global-set-key (kbd "C-x f") 'counsel-git)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
