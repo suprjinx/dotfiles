789816a5b0099e3ccfc61d7e0583084bf002f0cf
Geoff Wilson <geoff@gr-oss.io>
2026-07-07 12:22:38 -0400

    Fix some fish problems
    Signed-off-by: Geoff Wilson <geoff@gr-oss.io>

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,3 +5,10 @@ set -gx EDITOR "emacs"
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+for brew_path in /home/linuxbrew/.linuxbrew/bin/brew /opt/homebrew/bin/brew /usr/local/bin/brew
+    if test -x $brew_path
+        eval "$($brew_path shellenv fish)"
+        break
+    end
+end
